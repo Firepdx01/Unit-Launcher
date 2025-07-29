76369531fe740919e6981ceb8e44c5283f6fbe3d
Firepdx <96644284+Firepdx01@users.noreply.github.com>
2025-07-29 11:02:20 +0500

    hi

--- test.py
+++ test.py
@@ -1,13 +1,23 @@
-import time
+import tkinter as tk
 
-def countdown_timer(seconds):
-    while seconds:
+def start_timer():
+    try:
+        seconds = int(entry.get())
+        countdown(seconds)
+    except ValueError:
+        label.config(text="Please enter a valid number")
+
+def countdown(seconds):
+    if seconds >= 0:
         mins, secs = divmod(seconds, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        seconds -= 1
-    print("Time's up!")
+        time_format = '{:02d}:{:02d}'.format(mins, secs)
+        label.config(text=time_format)
+        root.after(1000, countdown, seconds - 1)
+    else:
+        label.config(text="Time's up!")
 
-# Example: start a timer for 10 seconds
-countdown_timer(10)
+# Create main window
+root = tk.Tk()
+root.title("Countdown Timer")
+
+# Inpu
